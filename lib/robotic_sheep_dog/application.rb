ca943cc4c06e5a4650013747c37cb07b9c4337c9
Matias Anaya <matiasanaya@gmail.com>
2014-09-21 19:23:25 +1000

    Implement acceptance test for application

---
+++ lib/robotic_sheep_dog/application.rb
@@ -0,0 +1,9 @@
+module RoboticSheepDog
+  class Application
+    def initialize(args)
+    end
+
+    def run
+    end
+  end
+end
---
+++ spec/features/robotic_sheep_dog_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../../lib/robotic_sheep_dog/application'
+
+RSpec.describe RoboticSheepDog::Application do
+
+  let(:app) { lambda { |f| RoboticSheepDog::Application.new(input: f).run } }
+
+  context 'when input is provided' do
+    it 'prints the correct output' do
+      input = <<-END.gsub(/^\s+\|/, '')
+        |5 5
+        |1 2 N
+        |LMLMLMLMM
+        |3 3 E
+        |MMRMMRMRRM
+      END
+
+      correct_output = <<-END.gsub(/^\s+\|/, '')
+        |1 3 N
+        |5 1 E
+      END
+
+      expect { app.call(input) }.to output(correct_output).to_stdout
+    end
+  end
+end
