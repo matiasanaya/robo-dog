e2b98cba2aa718dab3f7a16ec6bbe81b3bb7fa41
Matias Anaya <matiasanaya@gmail.com>
2014-09-23 10:22:04 +1000

    Add builder role to Paddock class

--- lib/robotic_sheep_dog/paddock.rb
+++ lib/robotic_sheep_dog/paddock.rb
@@ -1,5 +1,16 @@
 module RoboticSheepDog
   class Paddock
+
+    DataError = Class.new(StandardError)
+
+    def self.build(string)
+      raise DataError unless validate(string)
+
+      x, y = string.split(' ').map(&:to_i)
+
+      new(x, y)
+    end
+
     def initialize(x_size = 1, y_size = 1)
       @x_size = x_size
       @y_size = y_size
@@ -12,5 +23,9 @@ module RoboticSheepDog
     private
 
     attr_reader :x_size, :y_size
+
+    def self.validate(string)
+      string =~ /\A\d+ \d+\z/
+    end
   end
 end
--- spec/paddock_spec.rb
+++ spec/paddock_spec.rb
@@ -2,8 +2,31 @@ require_relative '../lib/robotic_sheep_dog/paddock'
 
 RSpec.describe RoboticSheepDog::Paddock do
   describe 'the public interface' do
+    it { expect(described_class).to respond_to :build }
+
     subject{ described_class.new }
     it { is_expected.to respond_to :valid_coordinates? }
+  end
+
+  describe '.build' do
+    context 'with invalid data' do
+      let(:data) { '1' }
+      it 'screams at you' do
+        expect { described_class.build(data) }.to raise_exception described_class::DataError
+      end
+    end
+    context 'with valid data' do
+      let(:data) { '4 3'}
+      it 'returns a paddock instance' do
+        expect(described_class.build(data)).to be_instance_of described_class
+      end
+      it 'builds with correct x size' do
+        expect(described_class.build(data).instance_variable_get(:@x_size)).to eql 4
+      end
+      it 'builds with correct y size' do
+        expect(described_class.build(data).instance_variable_get(:@y_size)).to eql 3
+      end
+    end
   end
 
   let(:paddock) { described_class.new(5, 5) }
