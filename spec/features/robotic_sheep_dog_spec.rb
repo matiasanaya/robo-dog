8e01049e4967f87cc24ba90cda2a4702b3191fde
Matias Anaya <matiasanaya@gmail.com>
2014-09-23 14:34:56 +1000

    Add acceptance tests.
    This includes tests for cases in which robots
    run over each other, or are placed on top of each other.

--- spec/features/robotic_sheep_dog_spec.rb
+++ spec/features/robotic_sheep_dog_spec.rb
@@ -5,22 +5,65 @@ RSpec.describe RoboticSheepDog::Application do
 
   let(:app) { lambda { |input_stream| RoboticSheepDog::Application.build(input_stream).run } }
 
-  context 'when input is provided' do
+  shared_examples 'a correct application' do |input_string, correct_output|
     it 'prints the correct output' do
-      input = <<-END.gsub(/^\s+\|/, '')
-        |5 5
-        |1 2 N
-        |LMLMLMLMM
-        |3 3 E
-        |MMRMMRMRRM
-      END
+      expect { app.call(StringIO.new(input_string)) }.to output(correct_output).to_stdout
+    end
+  end
 
-      correct_output = <<-END.gsub(/^\s+\|/, '')
-        |1 3 N
-        |5 1 E
-      END
+  shared_examples 'a complaining application' do |input_string, error|
+    it 'screams at the user' do
+      expect { app.call(StringIO.new(input_string)) }.to raise_exception error
+    end
+  end
 
-      expect { app.call(StringIO.new(input)) }.to output(correct_output).to_stdout
+  context 'when valid input is provided' do
+    context 'when robots do not run over each other' do
+      it_behaves_like 'a correct application',
+                      <<-END.gsub(/^\s+\|/, '') ,
+                        |5 5
+                        |1 2 N
+                        |LMLMLMLMM
+                        |3 3 E
+                        |MMRMMRMRRM
+                      END
+                      <<-END.gsub(/^\s+\|/, '')
+                        |1 3 N
+                        |5 1 E
+                      END
+    end
+    context 'when robots run over each other' do
+      it_behaves_like 'a complaining application',
+                      <<-END.gsub(/^\s+\|/, '') ,
+                        |5 5
+                        |0 0 E
+                        |M
+                        |1 0 N
+                        |L
+                      END
+                      RuntimeError
+    end
+
+    context 'when robots run over then end of the paddock' do
+      it_behaves_like 'a complaining application',
+                      <<-END.gsub(/^\s+\|/, '') ,
+                        |5 5
+                        |5 5 N
+                        |M
+                      END
+                      RuntimeError
+    end
+
+    context 'when robots are placed over each other' do
+      it_behaves_like 'a complaining application',
+                      <<-END.gsub(/^\s+\|/, '') ,
+                        |5 5
+                        |0 0 E
+                        |M
+                        |0 0 N
+                        |M
+                      END
+                      RuntimeError
     end
   end
 end
